ea0728ff99a34e525d8af7461775893c5184918f
PuskarRay <150375829+PuskarRay@users.noreply.github.com>
2026-07-27 17:58:15 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,42 @@ let someNumber = 33
 let stringNumber =String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//*****************************Operations**********************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let  s = 101 
+++s;
+console.log(s);
+
+
+//link to study 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
