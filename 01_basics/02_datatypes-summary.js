c86ba30f52cf39b4fa2d6135af928a8f4ac9a558
PuskarRay <150375829+PuskarRay@users.noreply.github.com>
2026-07-27 18:30:44 +0000

    datatypes summary

---
+++ 01_basics/02_datatypes-summary.js
@@ -0,0 +1,29 @@
+// Primitive 
+// 7 types : string ,number , boolean , null,  undefined , symbol,BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id ==  anotherId);
+
+const bigNumber = 6874366463164n
+
+//Reference(Non-primitive)
+
+//Array ,Objects , Functions 
+
+const heros =["saktiman","coder","loser"];
+
+
+//object
+let myObj = {
+    name:"hitesh",
+    age:22,
+}
+
+
+//function
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
