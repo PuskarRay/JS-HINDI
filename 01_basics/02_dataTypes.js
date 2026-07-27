8da5226b0adaab29f06d18a3ce9b2e6101273ca2
PuskarRay <150375829+PuskarRay@users.noreply.github.com>
2026-07-27 16:21:11 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS as a newer version
+
+//alert("hello") // we are using nodejs , not browser
+
+console.log(3+3);
+console.log("Hitesh");
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 ^ 53
+//bigint 
+//strings =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => uniqueness
+
+
+//object 
+
+
+console.log(typeof"hitesh");
+
+console.log(typeof null); // interview question (object)
+
